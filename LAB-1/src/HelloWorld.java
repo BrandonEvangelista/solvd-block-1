96b8d419c957ce83f6b70c99ded8b05f54a7f960
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-04-13 01:10:22 -0700

    Update HelloWorld.java

--- LAB-1/src/HelloWorld.java
+++ LAB-1/src/HelloWorld.java
@@ -1,10 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-
+        // intial hello statement
         System.out.println("Hello World!");
+
+        System.out.println("Let's Say hello to everyone!");
+
+        if (args.length == 0){
+            System.out.println("Awwww, No one showed up");
+        } else {
+            // prints "hello" to all names mentioned in parameters
+             for(int i = 0; i < args.length; i++) {
+                 System.out.println("Hello " + args[i] + "!");
+             }
+        }
 
 
     }
