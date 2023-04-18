89bb00e9c8df9f184c45cb1c261a4f5ea8d8ccd4
Brandon Evangelista <32882621+BrandonEvangelista@users.noreply.github.com>
2023-04-18 02:19:49 -0700

    update classes

--- LAB 3 class hierarchy/src/Airport.java
+++ LAB 3 class hierarchy/src/Airport.java
@@ -1,15 +1,15 @@
 class Airport {
-    String airportCode;
-    int numberOfGates;
-    int numberOfTerminals;
-    int numberOfRunways;
+    private String airportCode;
+    private int numberOfGates;
+    private  int numberOfTerminals;
+    private  int numberOfRunways;
 
     // constructor
     public Airport(String airportCode, int gate, int terminals, int runways) {
         this.airportCode = airportCode;
         this.numberOfGates = gate;
         this.numberOfTerminals = terminals;
-        this.numberOfRunways= runways;
+        this.numberOfRunways = runways;
     }
 
     // getters + setters
@@ -17,6 +17,7 @@ class Airport {
     public void setAirportCode(String code) {
         airportCode = code;
     }
+
     public String getAirportCode() {
         return airportCode;
     }
@@ -24,20 +25,23 @@ class Airport {
     public void setNumberOfGates(int gates) {
         numberOfGates = gates;
     }
+
     public int getNumberOfGates() {
         return numberOfGates;
     }
 
-    public void setNumberOfTerminals (int terminals) {
+    public void setNumberOfTerminals(int terminals) {
         numberOfGates = terminals;
     }
+
     public int getNumberOfTerminals() {
         return numberOfTerminals;
     }
 
-    public void setNumberOfRunways (int runway) {
+    public void setNumberOfRunways(int runway) {
         numberOfRunways = runway;
     }
+
     public int getNumberOfRunways() {
         return numberOfRunways;
     }
@@ -46,9 +50,9 @@ class Airport {
 
 
     public void printDetails() {
-       System.out.println("Airport Details: ");
-        System.out.println("   Airport Code: "+ getAirportCode());
-        System.out.println("   Number of gates: "+ getNumberOfGates() );
-        System.out.println("   Number of runways: "+ getNumberOfRunways());
+        System.out.println("Airport Details: ");
+        System.out.println("   Airport Code: " + getAirportCode());
+        System.out.println("   Number of gates: " + getNumberOfGates());
+        System.out.println("   Number of runways: " + getNumberOfRunways());
     }
 }
--- LAB 3 class hierarchy/src/Flight.java
+++ LAB 3 class hierarchy/src/Flight.java
@@ -1,19 +1,18 @@
 class Flight {
     private String flightNumber;
-    private String  arrivalTime;
+    private String arrivalTime;
     private String departureTime;
     private boolean isFlightDelayed;
     private String aiportOfOrigin;
     private String destinationAirport;
 
     // constructor;
-    public Airport(String flightNum, String arriveTime, String departTime, Boolean delay, String origin,String dest) {
-            this.flightNumber= flightNum;
-            this.arrivalTime = arriveTime;
-            this.departureTime = departTime
-        this.isFlightDelayed= delay;
-        this.aiportOfOrigin= origin;
-        this.destinationAirport= dest;
+    public Airport(String flightNum, String arriveTime, String departTime, Boolean delay, String origin, String dest) {
+        this.flightNumber = flightNum;
+        this.arrivalTime = arriveTime;
+        this.departureTime = departTime this.isFlightDelayed = delay;
+        this.aiportOfOrigin = origin;
+        this.destinationAirport = dest;
     }
 
     // getters + setters
@@ -21,27 +20,32 @@ class Flight {
     public void setFlightNumber(String code) {
         flightNumber = code;
     }
+
     public String getFlightNumber() {
         return flightNumber;
     }
 
     public void setNumberOfGates(int gates) {
+
         numberOfGates = gates;
     }
+
     public int getNumberOfGates() {
         return numberOfGates;
     }
 
-    public void setNumberOfTerminals (int terminals) {
+    public void setNumberOfTerminals(int terminals) {
         numberOfGates = terminals;
     }
+
     public int getNumberOfTerminals() {
         return numberOfTerminals;
     }
 
-    public void setNumberOfRunways (int runway) {
+    public void setNumberOfRunways(int runway) {
         numberOfRunways = runway;
     }
+
     public int getNumberOfRunways() {
         return numberOfRunways;
     }
@@ -51,8 +55,8 @@ class Flight {
 
     public void printDetails() {
         System.out.println("Airport Details: ");
-        System.out.println("   Airport Code: "+ getAirportCode());
-        System.out.println("   Number of gates: "+ getNumberOfGates() );
-        System.out.println("   Number of runways: "+ getNumberOfRunways());
+        System.out.println("   Airport Code: " + getAirportCode());
+        System.out.println("   Number of gates: " + getNumberOfGates());
+        System.out.println("   Number of runways: " + getNumberOfRunways());
     }
 }
